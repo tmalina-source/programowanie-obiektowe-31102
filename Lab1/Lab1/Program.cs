213b86da8df4bb2f93195357cc38cd08686f63d7
tmalina-source <tmalina@edu.cdv.pl>
2025-10-12 15:58:44 +0200

    Add files via upload

--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -1,3 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿//Zad2
+int number = 0;
+do
+{
+    Console.Write("Enter number: ");
+    string numberstring = Console.ReadLine();
+    bool success = int.TryParse(numberstring, out number);
 
-Console.WriteLine("Hello, World!");
+    if (!success)
+    {
+        Console.WriteLine("Provide an actual number");
+    }
+}
+while (number <= 0);
+
+//Zad1
+/*
+const string correctPassword = "admin123";
+string password;
+do
+{
+    Console.Write("Enter password: ");
+    password = Console.ReadLine();
+}
+while (password != correctPassword);
+Console.WriteLine("Succesfully loggen in!");
+*/
+//Początek lekcji
+/*
+const int requiredAgePartial = 14;
+const int requiredAgeFull = 18;
+const string accessDeniedMessage ="Access denied, You must be at least 14 years old for partial access or at least 18 for full access";
+const string accessAllowedPartial = "Partial access granted";
+const string accessAllowedFull = "Full access granted";
+
+int age;
+Console.WriteLine("Podaj swój wiek: ");
+
+string input = Console.ReadLine();
+
+bool success = int.TryParse(input, out age);
+
+if (!success)
+{
+    Console.WriteLine("Podaj poprawną wartość!");
+}
+else
+{
+    if (age >= requiredAgeFull)
+    {
+        Console.WriteLine(accessAllowedFull);
+    }
+    else if (age >= requiredAgePartial)
+    {
+        Console.WriteLine(accessAllowedPartial);
+    }
+    else
+    {
+        Console.WriteLine(accessDeniedMessage);
+    }
+}
+*/
---
+++ Lab2/Lab2/Program.cs
@@ -0,0 +1,80 @@
+﻿//Zad 6
+
+using System.Security.Cryptography.X509Certificates;
+
+class Zwierze
+{
+  public void Jedz() => Console.WriteLine("Zwierzę je");
+}
+class Pies : Zwierze
+{
+  public void Szczekaj() => Console.WriteLine("Hau hau!");
+}
+class Kot : Zwierze
+{
+  public void Miaucz() => Console.WrtiteLine("Miau miau!");
+}
+
+//Zad 5
+/*
+class KontoBankowe
+{
+  private double saldo;
+  public void Wplata(double kwota) { saldo += kwota; }
+  public double PobierzSaldo() { return saldo; }
+
+  public void Wyplata(double kwota)
+  {
+    if (kwota > saldo)
+    {
+      Console.Write("Insufficient funds");
+    }
+    else
+    {
+      saldo+= kwota;
+    }
+  }
+}
+*/
+/*
+Car car1 = new Car("Audi",2023);
+Car car2 = new Car("VW",1998);  
+Car car3 = new Car("Fiat",2011);
+
+car1.ShowMe();
+car2.ShowMe();
+car3.ShowMe();
+//car1.model = "test";
+class Car
+{
+  private string model;
+  private int year;
+
+  public string Model
+  {
+    get { return model; }
+    //set { model = value; }
+  }
+
+  public int Year
+  {
+    get { return year; }
+   // set if (value < 2000)
+   // {
+   // throw new Exception("Your car is too old"); 
+  //}
+  }
+  
+  public Car(string model, int year)
+  {
+    this.model=model;
+    this.year=year;
+  }
+
+  public void ShowMe()
+  {
+    Console.WriteLine($"Model: {model}");
+    Console.WriteLine($"Year: {year}");
+  }
+}
+*/
